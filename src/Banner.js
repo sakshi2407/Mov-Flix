bed44fe89241c88bc743bd75c16f0eec58f3fe45
sakshi2407 <lcs2021020@iiitl.ac.in>
2023-04-22 14:39:31 +0530

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <header 
+    className='banner'
+    style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+        backgroundPosition: "center center",
+    }}
+    >
+        <div className="banner__contents">
+            <h1 className='banner__title'>Movie Name</h1>
+            <div className="banner__buttons">
+              <button className='banner__button'>Play</button>
+              <button className='banner__button'>My List</button>
+            </div>
+            <div className="banner__description">
+              This is a test description
+            </div>
+        </div>
+        <div className="banner--fadeButton" />
+    </header>
+  );
+  
+}
+
+export default Banner
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBBQM6u0g7IK1-T-AiHWcShsNIBujtZydw",
+    authDomain: "netflix-clone-85d1e.firebaseapp.com",
+    projectId: "netflix-clone-85d1e",
+    storageBucket: "netflix-clone-85d1e.appspot.com",
+    messagingSenderId: "858984069959",
+    appId: "1:858984069959:web:a890372ede5565e7366a62"
+  };
